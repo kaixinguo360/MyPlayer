90ea75928c40e9ec1412e3f6bd7bd969c36876a5
Kaixinguo <kaixinguo@kaixinguo.site>
2021-01-12 17:37:31 +0800

    Update At 2021-01-12-17:37:31

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="d-player" class="d-player"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dplayer from 'dplayer'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  mounted() {
+    const dp = new Dplayer({
+      container: document.getElementById('d-player'),
+      video: {
+        url: 'https://jellyfin.kaixinguo.site/Items/e513c1b7a1f57cf0ec462ec4ac3cf120/Download?api_key=d54cc08c64ef4465a0fc55e56425a90c',
+      },
+    });
+    console.log(dp)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.d-player {
+  width: 300px;
+  height: 200px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+window.Hls = require('hls.js');
+window.Dash = require('dash.js');
 
 new Vue({
   render: h => h(App),
